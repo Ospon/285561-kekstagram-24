e7cd5f5d560abb3f91bac6f39fd500a77c32ee7d
Ospon <ospania@gmail.com>
2021-10-03 19:31:44 +0200

    Module 4 task 1 work in progress

--- js/main.js
+++ js/main.js
@@ -1,27 +1,61 @@
-// Получение случайного целого числа в заданном интервале
+// Получение случайного целого числа в заданном интервале, включительно
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-function getRandomInt(min, max) {
+function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
+getRandomIntInclusive(1, 10);
 
-getRandomInt(1, 10);
+const checkStringLength = (stringValue, stringMaxLength) => stringValue.length < stringMaxLength;
+checkStringLength('Hello world', 20);
 
-// Проверка работоспособности:
-// console.log(getRandomInt(1, 10));
+const commentNames = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
 
-const getStringLength = (stringValue, stringMaxLength) => stringValue.length < stringMaxLength;
+const photoDescriptions = [
+  'Description-1',
+  'Description-2',
+  'Description-3',
+  'Description-4',
+  'Description-5',
+  'Description-6',
+];
 
-getStringLength('Hello world', 20);
+const generatedItemsCount = 25;
+const likesMin = 15;
+const likesMax = 250;
 
-// Алтернативное написание:
-// function getStringLength(stringValue, stringMaxLength) {
-//   return stringValue.length < stringMaxLength;
-// }
+const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
 
-// Проверка работоспособности:
-// console.log(getStringLength('Hello world', 20));
+const generateComment = function() {
+  return {
+    id: getRandomIntInclusive(1, generatedItemsCount),
+    avatar: 'img/avatar/-' + getRandomIntInclusive(1, generatedItemsCount) + '.svg',
+    message: 'Generated',
+    name: getRandomArrayElement(commentNames),
+  };
+};
 
+const generateObject = function() {
 
+  return {
+    id: getRandomIntInclusive(1, generatedItemsCount),
+    url: 'photos/' + getRandomIntInclusive(1, generatedItemsCount) + '.jpg',
+    description: getRandomArrayElement(photoDescriptions),
+    likes: getRandomIntInclusive(likesMin, likesMax),
+    comments: generateComment(),
+  };
+};
+
+const items = Array.from({length: generatedItemsCount}, generateObject);
+//console.log(items);
