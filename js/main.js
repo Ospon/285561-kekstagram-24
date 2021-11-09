4736452a6913a6ca5038885909626af87f34cfe8
Konstantin Torkhov <ospania@gmail.com>
2021-11-09 20:50:24 +0100

    Code refactored

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-//import './utils/get-random-positive-integer.js';
 import './modules/form-image-upload.js';
 import { debounce } from './utils/debounce.js';
 import './modules/live-image-preview.js';
-import './modules/post-preview.js';
+import { openPostPreview, setOpenPostPreviewClick} from './modules/post-preview.js';
 
 import { renderPosts, setDefaultFilterButtonClick, renderMostDiscussedPosts, setDiscussedFilterButtonClick, renderRandomPosts, setRandomFilterButtonClick } from './modules/users-pictures.js';
 import { getData } from './modules/api.js';
@@ -11,6 +10,7 @@ import { showAlert } from './utils/show-alert.js';
 getData((pictures) => {
   renderPosts(pictures);
   setDiscussedFilterButtonClick(debounce(() => renderMostDiscussedPosts(pictures)));
-  setDefaultFilterButtonClick(debounce(() =>renderPosts(pictures)));
+  setDefaultFilterButtonClick(debounce(() => renderPosts(pictures)));
   setRandomFilterButtonClick(debounce(() => renderRandomPosts(pictures)));
+  setOpenPostPreviewClick((evt) => openPostPreview(pictures, evt));
 }, showAlert);
